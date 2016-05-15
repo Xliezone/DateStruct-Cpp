e7e8e5871f6ecee23d7a74398c0671440e5ae3e4
Xliezone <xliez@foxmail.com>
2016-05-15 23:15:19 +0800

    update

--- DataStruct/Tree Traversals Again.cpp
+++ DataStruct/Tree Traversals Again.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+vector<int> preOrder;
+vector<int> inOrder;
+
 void Read(vector<int> &preOrder, vector<int> &inOrder) {
     int N;
     cin >> N;
@@ -26,21 +29,52 @@ void Read(vector<int> &preOrder, vector<int> &inOrder) {
     }
 }
 
-struct Node {
+typedef struct TNode {
     int data;
-    int left;
-    int right;
-};
+    TNode *left, *right;
+}* Node;
 
-typedef vector<Node> Tree;
+void buildTree1(Node &T, int PreIndex, int InIndex, int len) {
+    //字符串长度为0时，返回上一层
+    if (len <= 0) {
+        T = NULL;
+        return;
+    }
+    T = (Node ) malloc(sizeof(TNode));
+    //存节点
+    T->data = preOrder[PreIndex];
+    //找出节点在中序序列中的位置
+    int p;
+    for (int i = 0; i < inOrder.size(); ++i) {
+        if (inOrder[i] == preOrder[PreIndex]) {
+            p = i;
+        }
+    }
+    //左子树的长度
+    int LenF = p - InIndex;
+    //建立左子树
+    buildTree1(T->left, PreIndex + 1, InIndex, LenF);
+    //右子树的长度
+    int LenR = len - 1 - LenF;
+    //建立右子树
+    buildTree1(T->right, PreIndex + LenF + 1, p + 1, LenR);
+}
 
-void Build() {
-
+void Print(Node &tree, int count) {
+    if (tree != 0) {
+        Print(tree->left, 1);
+        Print(tree->right, 1);
+        if (count == 0)
+            cout << tree->data;
+        else
+            cout << tree->data << " ";
+    }
 }
 
 int main() {
-    vector<int> preOrder;
-    vector<int> inOrder;
     Read(preOrder, inOrder);
+    Node root;
+    buildTree1(root, 0, 0, preOrder.size());
+    Print(root, 0);
     return 0;
 }
