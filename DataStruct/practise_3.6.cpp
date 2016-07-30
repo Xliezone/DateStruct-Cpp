c3ec0ad1a14a9615a625500f3f5d71cb2c6fb665
xliez <xliez@foxmail.com>
2016-07-30 12:52:58 +0800

    up

---
+++ DataStruct/practise_3.6.cpp
@@ -0,0 +1,23 @@
+//practise_3.6
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node {
+    int value;
+    Node *next = 0;
+} *LoopList;
+
+void josephus(int M, int N) {
+    LoopList head = new Node;
+    int n = 1;
+    LoopList p = head;
+    while (n != N) {
+        
+    }
+}
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
