7d745d788f7702be6572499d956b4034db97890f
xliez <xliez@foxmail.com>
2016-07-31 12:12:19 +0800

    update

--- DataStruct/practise_3.6.cpp
+++ DataStruct/practise_3.6.cpp
@@ -1,23 +1,53 @@
 //practise_3.6
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 typedef struct Node {
+    Node *prev = 0;
     int value;
     Node *next = 0;
 } *LoopList;
 
-void josephus(int M, int N) {
-    LoopList head = new Node;
+vector<int> josephus(int M, int N) {
+    LoopList p, head;
+    head = new Node;
+    p = head;
     int n = 1;
-    LoopList p = head;
-    while (n != N) {
-        
+    while (n <= N) {
+        p->next = new Node;
+        p->next->prev = p;
+        p = p->next;
+        p->value = n;
+        n++;
     }
+    p->next = head->next;
+    head->next->prev = p;
+    int m = 0;
+    int left = N;
+    p = head->next;
+    vector<int> out;
+    while (left != 0) {
+        if (m != M) {
+            p = p->next;
+            m++;
+        }
+        else{
+            m = 0;
+            out.push_back(p->value);
+            p->prev->next = p->next;
+            p->next->prev = p->prev;
+            LoopList temp = p;
+            p = p->next;
+            delete temp;
+            left--;
+        }
+    }
+    return out;
 }
 
 int main(int argc, char const *argv[]) {
-
+    vector<int> out = josephus(1, 5);
     return 0;
 }
