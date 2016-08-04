8bff86c2acdf52b3a8d56b77e277f8ef9fb9298a
xliez <xliez@foxmail.com>
2016-08-05 01:02:47 +0800

    update

--- DataStruct/practise_3.24.cpp
+++ DataStruct/practise_3.24.cpp
@@ -4,22 +4,24 @@
 using namespace std;
 
 int main(int argc, char const *argv[]){
-    int length;
-    int stack[length];
-    size_t top1 = 0;
-    size_t top2 = length-1;
+	int length;
+	int stack[length];
+	size_t top1 = 0;
+	size_t top2 = length-1;
     //push
-    int number;
-    stack[top1] = number;
-    top1++;
-    stack[top2] = number;
-    top2--;
+	int number;
+	if (top1 <= top2) {
+		stack[top1] = number;
+		top1++;
+		stack[top2] = number;
+		top2--;
+	}
     //pop
-    stack[top1] = 0;
-    top1--;
-    stack[top2] = 0;
-    top2++;
+	stack[top1] = 0;
+	top1--;
+	stack[top2] = 0;
+	top2++;
     //get
     // stack[top1] || stack[top2]
-    return 0;
+	return 0;
 }
